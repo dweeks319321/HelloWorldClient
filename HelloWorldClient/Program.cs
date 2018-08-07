426cc4d44fd3b6a43561bd9e352e1f8e3eea2d37
dweek <dweek@DESKTOP-GH24S7G>
2018-08-06 20:10:10 -0400

    Commit Hello World Changes

---
+++ HelloWorldClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+            string text = client.DownloadString("http://localhost:61395//api/ReturnTextAPI/GetReturnText");
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
